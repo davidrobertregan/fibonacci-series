4cc09a187c92abe6a4e858bae5200722b961df76
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-10-14 15:54:23 -0500

    commit tree 23f7e662aafde71c6f0e12f0512f4aa19a02a9b8

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,11 +1,31 @@
 function fibonacci(num) {
-  // type your code here
+  let fibArr = [0, 1, 1]
+
+  if(num < 3) {
+    return fibArr[num]
+  }
+  
+  for(i = fibArr.length; i < num + 1; i++) {
+    const element = fibArr[i - 1] + fibArr[i - 2]
+      fibArr.push(element)
+    }
+
+  return fibArr[fibArr.length - 1]
 }
 
 if (require.main === module) {
   // add your own tests in here
+
+  console.log("Expecting: 2");
+  console.log("=>", fibonacci(3));
+
+  console.log("Expecting: 8");
+  console.log("=>", fibonacci(6));
+
   console.log("Expecting: 0");
   console.log("=>", fibonacci(0));
+
+  
 
   console.log("");
 
@@ -22,3 +42,9 @@ module.exports = fibonacci;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// fib takes a num as an argument. the function will then return the nth value based on the number passed in. ie fib(10) will return the 10th element.
+
+// create an array thats the same length as the number... 
+// create a loop that will iterate as long as the number 
+// 
